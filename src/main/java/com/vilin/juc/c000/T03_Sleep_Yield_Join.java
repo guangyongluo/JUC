6e77b666066a4357338559644e76d8279f99e0a9
guangyongluo(罗葳) <llooww@mail.ustc.edu.cn>
2020-08-01 10:43:36 +0800

    JUC(com.vilin.juc.c000):thread basic concept.

---
+++ src/main/java/com/vilin/juc/c000/T03_Sleep_Yield_Join.java
@@ -0,0 +1,70 @@
+package com.vilin.juc.c000;
+
+public class T03_Sleep_Yield_Join {
+    static void testSleep(){
+        new Thread(() -> {
+           for(int i = 0; i < 10; i++){
+               System.out.println("A" + i);
+
+               try {
+                   Thread.sleep(500);
+               } catch (InterruptedException e) {
+                   e.printStackTrace();
+               }
+           }
+        }).start();
+    }
+
+    static void testYield(){
+        new Thread(() -> {
+            for(int i = 0; i < 100; i++){
+                System.out.println("A" + i);
+                if(i % 10 == 0) Thread.yield();
+            }
+        }).start();
+
+        new Thread(() -> {
+            for(int i = 0; i < 100; i++){
+                System.out.println("---------B" + i);
+                if(i % 10 == 0) Thread.yield();
+            }
+        }).start();
+    }
+
+    static void testJoin(){
+        Thread t1 = new Thread(() -> {
+            for(int i = 0; i < 10; i++){
+                System.out.println("A" + i);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            try {
+                t1.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            for(int i = 0; i < 10; i++){
+                System.out.println("A" + i);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
+
+    public static void main(String[] args) {
+        testJoin();
+    }
+}
