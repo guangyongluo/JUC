50ef2630de0033bdf75f8d3bc54f5be74267251c
guangyongluo(罗葳) <llooww@mail.ustc.edu.cn>
2021-02-21 21:03:24 +0800

    JUC(com.vilin.juc.chapter03):thread dead lock.

---
+++ src/main/java/com/vilin/juc/chapter08/DeadLockTest.java
@@ -0,0 +1,27 @@
+package com.vilin.juc.chapter08;
+
+public class DeadLockTest {
+    public static void main(String[] args) {
+        OtherService otherService = new OtherService();
+        DeadLock deadLock = new DeadLock(otherService);
+        otherService.setDeadLock(deadLock);
+
+        new Thread("T1"){
+            @Override
+            public void run() {
+                while (true){
+                    deadLock.m1();
+                }
+            }
+        }.start();
+
+        new Thread("T2"){
+            @Override
+            public void run() {
+                while (true){
+                    otherService.s2();
+                }
+            }
+        }.start();
+    }
+}
