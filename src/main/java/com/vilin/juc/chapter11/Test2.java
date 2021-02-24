d4019fff493036bf04c8c6e4cfc0cc9de040005f
guangyongluo(罗葳) <llooww@mail.ustc.edu.cn>
2021-02-24 14:28:24 +0800

    JUC(com.vilin.juc.chapter11):thread stack and catch thread exception.

---
+++ src/main/java/com/vilin/juc/chapter11/Test2.java
@@ -0,0 +1,15 @@
+package com.vilin.juc.chapter11;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public class Test2 {
+
+    public void test(){
+        Arrays.asList(Thread.currentThread().getStackTrace()).stream()
+                .filter(e -> !e.isNativeMethod())
+                .forEach(e -> Optional.of(e.getClassName() + "" + e.getMethodName() + ":" + e.getLineNumber())
+                .ifPresent(System.out::println));
+    }
+
+}
---
+++ src/main/java/com/vilin/juc/chapter11/ThreadException.java
@@ -0,0 +1,28 @@
+package com.vilin.juc.chapter11;
+
+public class ThreadException {
+
+    private final static int A = 10;
+    private final static int B = 0;
+
+    public static void main(String[] args) {
+        Thread t = new Thread(() -> {
+            try {
+                Thread.sleep(5_000);
+                int result = A / B;
+                System.out.println(result);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        t.setUncaughtExceptionHandler((thread, e) -> {
+            System.out.println(e);
+            System.out.println(thread);
+        });
+
+        t.start();
+
+//        new Test1().test();
+    }
+}
