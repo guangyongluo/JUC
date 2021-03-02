2d04e00851dc7ea1cb7cae65eb2292074d1f208d
guangyongluo(罗葳) <llooww@mail.ustc.edu.cn>
2021-03-02 18:13:08 +0800

    JUC(com.vilin.concurrent.chapter03):volatile key word.

---
+++ src/main/java/com/vilin/concurrent/chapter03/VolatileTest2.java
@@ -0,0 +1,32 @@
+package com.vilin.concurrent.chapter03;
+
+public class VolatileTest2 {
+
+    private volatile static int INIT_VALUE = 0;
+
+    private final static int MAX_LIMIT = 500;
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            while(INIT_VALUE < MAX_LIMIT){
+                System.out.println("T1 -> " + (++INIT_VALUE));
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "ADDER-1").start();
+
+        new Thread(() -> {
+            while(INIT_VALUE < MAX_LIMIT){
+                System.out.println("T2 -> " + (++INIT_VALUE));
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "ADDER-2").start();
+    }
+}
