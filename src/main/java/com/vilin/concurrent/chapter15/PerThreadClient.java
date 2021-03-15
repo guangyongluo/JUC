b6e3cd4be8da27412d851c73862825ec3e8060be
guangyongluo(罗葳) <llooww@mail.ustc.edu.cn>
2021-03-15 15:05:08 +0800

    JUC(com.vilin.concurrent.chapter14):pre thread design pattern.

---
+++ src/main/java/com/vilin/concurrent/chapter15/PerThreadClient.java
@@ -0,0 +1,12 @@
+package com.vilin.concurrent.chapter15;
+
+import java.util.stream.IntStream;
+
+public class PerThreadClient {
+
+    public static void main(String[] args) {
+        MessageHandler handler = new MessageHandler();
+        IntStream.rangeClosed(0, 10).forEach(i -> handler.request(new Message(String.valueOf(i))));
+        handler.shutdown();
+    }
+}
